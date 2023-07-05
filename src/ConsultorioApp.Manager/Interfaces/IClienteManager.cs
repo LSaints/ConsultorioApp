3b80bb7dc0c5c07484c7f260b3f5bd0efff31f75
LSaints <mateus.dev@gmail.com>
2023-07-05 10:33:31 -0300

    Adicionado Interfaces

---
+++ src/ConsultorioApp.Manager/Interfaces/IClienteManager.cs
@@ -0,0 +1,10 @@
+﻿using ConsultorioApp.Core.Domain;
+
+namespace ConsultorioApp.Data.Repository
+{
+    public interface IClienteManager
+    {
+        Task<Cliente> GetClienteAsync(int id);
+        Task<IEnumerable<Cliente>> GetClientesAsync();
+    }
+}
---
+++ src/ConsultorioApp.Manager/Interfaces/IClienteRepository.cs
@@ -0,0 +1,10 @@
+﻿using ConsultorioApp.Core.Domain;
+
+namespace ConsultorioApp.Manager.Interfaces
+{
+    public interface IClienteRepository
+    {
+        Task<Cliente> GetClienteAsync(int id);
+        Task<IEnumerable<Cliente>> GetClientesAsync();
+    }
+}
