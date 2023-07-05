9efd39900c845425de2508791c75be885af3241b
LSaints <mateus.dev@gmail.com>
2023-07-05 11:56:57 -0300

    Adicionado campos em cliente

--- src/ConsultorioApp.Core/Domain/Cliente.cs
+++ src/ConsultorioApp.Core/Domain/Cliente.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime DataNascimento { get; set; }
+        public char Sexo { get; set; }
+        public string Telefone { get; set; }
+        public string Documento { get; set; }
 
     }
 }
