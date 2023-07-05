98ab35563b25be869dd077caf7b4c315563b80ac
LSaints <mateus.dev@gmail.com>
2023-07-05 10:34:28 -0300

    Implementado Interfaces de repository e ClienteManager

---
+++ src/ConsultorioApp.Data/Implementation/ClienteManager.cs
@@ -0,0 +1,25 @@
+﻿using ConsultorioApp.Core.Domain;
+using ConsultorioApp.Data.Repository;
+using ConsultorioApp.Manager.Interfaces;
+
+namespace ConsultorioApp.Data.Implementation
+{
+    public class ClienteManager : IClienteManager
+    {
+        private readonly IClienteRepository _repository;
+        public ClienteManager(IClienteRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Cliente>> GetClientesAsync()
+        {
+            return await _repository.GetClientesAsync();
+        }
+
+        public async Task<Cliente> GetClienteAsync(int id)
+        {
+            return await _repository.GetClienteAsync(id);
+        }
+    }
+}
---
+++ src/ConsultorioApp.Data/Repository/ClienteRepository.cs
@@ -0,0 +1,27 @@
+﻿using ConsultorioApp.Core.Domain;
+using ConsultorioApp.Data.Context;
+using ConsultorioApp.Manager.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsultorioApp.Data.Repository
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly ConsultorioAppContext _context;
+        public ClienteRepository(ConsultorioAppContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Cliente>> GetClientesAsync()
+        {
+            return await _context.Clientes.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Cliente> GetClienteAsync(int id)
+        {
+            return await _context.Clientes.FindAsync(id);
+        }
+
+    }
+}
