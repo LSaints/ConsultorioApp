540d3816df6325672ae4f5c24e3d7de068357ad1
LSaints <mateus.dev@gmail.com>
2023-07-05 19:32:31 -0300

    Adicionado Mappings para os metodos de INSERT e PUT de cliente

---
+++ src/ConsultorioApp.Manager/Validator/AlteraClienteValidator.cs
@@ -0,0 +1,15 @@
+﻿using ConsultorioApp.Shared.ModelView;
+using FluentValidation;
+using System.Data;
+
+namespace ConsultorioApp.Manager.Validator
+{
+    public class AlteraClienteValidator : AbstractValidator<AlteraCliente>
+    {
+        public AlteraClienteValidator()
+        {
+            RuleFor(c => c.Id).NotNull().NotEmpty().GreaterThan(0);
+            Include(new NovoClienteValidator());
+        }
+    }
+}
--- src/ConsultorioApp.Manager/Validator/NovoClienteValidator.cs
+++ src/ConsultorioApp.Manager/Validator/NovoClienteValidator.cs
@@ -1,16 +1,17 @@
 ﻿using ConsultorioApp.Core.Domain;
+using ConsultorioApp.Shared.ModelView;
 using FluentValidation;
 
 namespace ConsultorioApp.Manager.Validator
 {
-    public class ClienteValidator : AbstractValidator<Cliente>
+    public class NovoClienteValidator : AbstractValidator<NovoCliente>
     {
-        public ClienteValidator()
+        public NovoClienteValidator()
         {
             RuleFor(x => x.Name).NotEmpty().NotNull().MinimumLength(10).MaximumLength(150);
             RuleFor(x => x.DataNascimento).NotEmpty().NotNull().LessThan(DateTime.Now).GreaterThan(DateTime.Now.AddYears(-130));
             RuleFor(x => x.Documento).NotEmpty().NotNull().MinimumLength(4).MaximumLength(14);
-            RuleFor(x => x.Telefone).NotEmpty().NotNull().Matches("[2-9][6-9]{10}").WithMessage("O telefone tem que ter o formato [2-9][0-9]{10}");
+            RuleFor(x => x.Telefone).NotEmpty().NotNull().Matches("[2-9][0-9]{10}").WithMessage("O telefone tem que ter o formato [2-9][0-9]{10}");
             RuleFor(x => x.Sexo).NotEmpty().NotNull().Must(IsMorF).WithMessage("Sexo precisa ser M ou F");
         }
 
