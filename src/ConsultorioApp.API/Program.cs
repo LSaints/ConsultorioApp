ad372b428665207745327d82dee6f490bcf1d730
LSaints <mateus.dev@gmail.com>
2023-07-05 10:35:32 -0300

    Implementado Injeção de independencia

--- src/ConsultorioApp.API/Program.cs
+++ src/ConsultorioApp.API/Program.cs
@@ -1,4 +1,7 @@
 using ConsultorioApp.Data.Context;
+using ConsultorioApp.Data.Implementation;
+using ConsultorioApp.Data.Repository;
+using ConsultorioApp.Manager.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 internal class Program
@@ -11,8 +14,12 @@ internal class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        
         builder.Services.AddDbContext<ConsultorioAppContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("ConsultorioConnection")));
+
+        builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+        builder.Services.AddScoped<IClienteManager, ClienteManager>();
 
         var app = builder.Build();
 
