10c9fbcec9f9b3dd0b8c8203842151da0ced8c56
LSaints <mateus.dev@gmail.com>
2023-07-05 11:55:28 -0300

    adicionado novas migrations

---
+++ src/ConsultorioApp.Data/Migrations/20230705143700_AdicionaCamposCliente.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsultorioApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionaCamposCliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Documento",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sexo",
+                table: "Clientes",
+                type: "nvarchar(1)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telefone",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Documento",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "Sexo",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "Telefone",
+                table: "Clientes");
+        }
+    }
+}
