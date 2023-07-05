eb51ecc62e895d6083cf6fee80cec489ff870f28
LSaints <mateus.dev@gmail.com>
2023-07-05 19:34:02 -0300

    Adicionado novas Migrations

---
+++ src/ConsultorioApp.Data/Migrations/20230705215235_AdicionandoDataDeControle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsultorioApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoDataDeControle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Criacao",
+                table: "Clientes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UltimaAtualizacao",
+                table: "Clientes",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Criacao",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "UltimaAtualizacao",
+                table: "Clientes");
+        }
+    }
+}
