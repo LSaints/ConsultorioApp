3f7c81b621c522ece1b97ba40b11aca25a2f4691
LSaints <mateus.dev@gmail.com>
2023-07-05 11:57:32 -0300

    adicionado FluentValidation com Injeção independencia

--- src/ConsultorioApp.API/Program.cs
+++ src/ConsultorioApp.API/Program.cs
@@ -2,40 +2,43 @@ using ConsultorioApp.Data.Context;
 using ConsultorioApp.Data.Implementation;
 using ConsultorioApp.Data.Repository;
 using ConsultorioApp.Manager.Interfaces;
+using ConsultorioApp.Manager.Validator;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
-internal class Program
-{
-    private static void Main(string[] args)
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ConsultorioAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConsultorioConnection")));
+
+builder.Services.AddControllers()
+    .AddFluentValidation(p =>
     {
-        var builder = WebApplication.CreateBuilder(args);
-        
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        
-        builder.Services.AddDbContext<ConsultorioAppContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("ConsultorioConnection")));
+        p.RegisterValidatorsFromAssemblyContaining<ClienteValidator>();
+        p.ValidatorOptions.LanguageManager.Culture = new CultureInfo("pt-BR");
+    });
 
-        builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
-        builder.Services.AddScoped<IClienteManager, ClienteManager>();
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<IClienteManager, ClienteManager>();
 
-        var app = builder.Build();
+var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
