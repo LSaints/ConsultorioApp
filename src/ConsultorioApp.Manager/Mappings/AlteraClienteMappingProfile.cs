fe5bfa3afc2aeae09d9627d166ae3adb8c630869
LSaints <mateus.dev@gmail.com>
2023-07-05 19:32:52 -0300

    Mapping para cliente

---
+++ src/ConsultorioApp.Manager/Mappings/AlteraClienteMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ConsultorioApp.Core.Domain;
+using ConsultorioApp.Shared.ModelView;
+
+namespace ConsultorioApp.Manager.Mappings
+{
+    public class AlteraClienteMappingProfile : Profile
+    {
+        public AlteraClienteMappingProfile()
+        {
+            CreateMap<AlteraCliente, Cliente>()
+                .ForMember(c => c.UltimaAtualizacao, o => o.MapFrom(x => DateTime.Now))
+                .ForMember(c => c.DataNascimento, o => o.MapFrom(x => x.DataNascimento.Date));
+        }
+    }
+}
---
+++ src/ConsultorioApp.Manager/Mappings/NovoClienteMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ConsultorioApp.Core.Domain;
+using ConsultorioApp.Shared.ModelView;
+
+namespace ConsultorioApp.Manager.Mappings
+{
+    public class NovoClienteMappingProfile : Profile
+    {
+        public NovoClienteMappingProfile() 
+        {
+            CreateMap<NovoCliente, Cliente>()
+                .ForMember(d => d.Criacao, o => o.MapFrom(x => DateTime.Now))
+                .ForMember(d => d.DataNascimento, o => o.MapFrom(x => x.DataNascimento.Date));
+        }
+    }
+}
