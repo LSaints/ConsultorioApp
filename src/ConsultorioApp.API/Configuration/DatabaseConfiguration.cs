cbc7b2e4a30736f47336e45ea97b856e2bfc56f5
LSaints <mateus.dev@gmail.com>
2023-07-06 13:22:36 -0300

    adicionado DatabaseConfiguratio

---
+++ src/ConsultorioApp.API/Configuration/DatabaseConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using ConsultorioApp.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsultorioApp.API.Configuration
+{
+    public static class DatabaseConfiguration
+    {
+        public static void AddDatabaseConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ConsultorioAppContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("ConsultorioConnection")));
+        }
+
+        public static void UseDatabaseConfiguration(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            using var context = serviceScope.ServiceProvider.GetService<ConsultorioAppContext>();
+            context.Database.Migrate();
+            context.Database.EnsureCreated();
+        }
+    }
+}
--- src/ConsultorioApp.API/Program.cs
+++ src/ConsultorioApp.API/Program.cs
@@ -1,35 +1,40 @@
 using ConsultorioApp.API.Configuration;
-using ConsultorioApp.Data.Context;
-using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.UseDependecyInjectionConfiguration();
-
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ConsultorioAppContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConsultorioConnection")));
-
-builder.Services.UseAutoMapperConfiguration();
-
-builder.Services.UseFluentValidationConfiguration();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.UseDependecyInjectionConfiguration();
+
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDatabaseConfiguration(builder.Configuration);
+
+        builder.Services.UseAutoMapperConfiguration();
+
+        builder.Services.UseFluentValidationConfiguration();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseDatabaseConfiguration();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
