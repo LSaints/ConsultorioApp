4816dcc46864b8b3482e36f097dfb4dd73d3d66f
LSaints <mateus.dev@gmail.com>
2023-07-07 09:51:03 -0300

    Adicionado Logs

---
+++ src/ConsultorioApp.API/Configuration/SerilogConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Serilog;
+
+namespace ConsultorioApp.API.Configuration
+{
+    public static class SerilogConfiguration
+    {
+        public static void UseSerilogConfiguration(this IHostBuilder host)
+        {
+            host.UseSerilog((context, configuration) =>
+                configuration.ReadFrom.Configuration(context.Configuration));
+        }
+    }
+}
--- src/ConsultorioApp.API/Controllers/ClienteController.cs
+++ src/ConsultorioApp.API/Controllers/ClienteController.cs
@@ -12,9 +12,12 @@ namespace ConsultorioApp.API.Controllers
     public class ClienteController : ControllerBase
     {
         private readonly IClienteManager _manager;
-        public ClienteController(IClienteManager manager)
+        private readonly ILogger<ClienteController> _logger;
+        public ClienteController(IClienteManager manager, ILogger<ClienteController> logger)
         {
             _manager = manager;
+            _logger = logger;
+            _logger.LogInformation("Controller de cliente foi chamado");
         }
 
         // GET: api/<ClienteController>
@@ -23,6 +26,7 @@ namespace ConsultorioApp.API.Controllers
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult> Get()
         {
+            _logger.LogInformation("foi chamado o metodo GET de ClienteController");
             return Ok(await _manager.GetClientesAsync());
         }
 
@@ -32,6 +36,7 @@ namespace ConsultorioApp.API.Controllers
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult> Get(int id)
         {
+            _logger.LogInformation("foi chamado o metodo GET de ClienteController");
             return Ok(await _manager.GetClienteAsync(id));
         }
 
@@ -41,6 +46,7 @@ namespace ConsultorioApp.API.Controllers
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
         public async Task<IActionResult> Post([FromBody] NovoCliente cliente)
         {
+            _logger.LogInformation("foi chamado o metodo POST de ClienteController");
             var clienteInserido = await _manager.InsertClienteAsync(cliente);
             return CreatedAtAction(nameof(Get), new {id = clienteInserido.Id }, clienteInserido);
         }
@@ -52,7 +58,7 @@ namespace ConsultorioApp.API.Controllers
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult> Put([FromBody] AlteraCliente cliente)
         {
-
+            _logger.LogInformation("foi chamado o metodo PUT de ClienteController");
             var clienteAtualizado = await _manager.UpdateClienteAsync(cliente);
             if (clienteAtualizado == null)
             {
@@ -68,6 +74,7 @@ namespace ConsultorioApp.API.Controllers
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult> Delete(int id)
         {
+            _logger.LogInformation("foi chamado o metodo DELETE de ClienteController");
             await _manager.DeleteClienteAsync(id);
             return NoContent();
         }
